a7031de85f4eabcbea558423c7af600abe128fca
alt-franz <examdeskseries@gmail.com>
2024-01-18 21:39:59 +0000

    mobile design

--- src/components/navigation/AppNavBar.tsx
+++ src/components/navigation/AppNavBar.tsx
@@ -12,23 +12,23 @@ export const AppNavBar = async () => {
 
   return (
     <nav
-      className='flex items-center justify-between p-6 lg:px-8 fixed w-full'
+      className='flex items-center justify-between p-6 lg:px-8 md:fixed w-full'
       aria-label='Global'
     >
       <div className='flex lg:flex-1'>
         <Link href='/' className='-m-1.5 p-1.5'>
-          <span className='sr-only'>360NFT</span>
+          <span className='sr-only'>Trapcard</span>
           <img
-            className='h-8 w-auto backdrop-blur-[1px]'
+            className='md:h-8 md:w-auto w-32 backdrop-blur-[1px]'
             src='/images/TrapCard.png'
             alt='Trapcard_LOGO'
           />
         </Link>
       </div>
 
-      <div className='flex gap-x-8 items-center'>
-        <ThemeButton />
-        {session?.user ? <Link href={'/account/referral'} className='rounded-xl bg-white py-2.5 px-3 font-semibold font-mont '>
+      <div className='flex md:gap-x-8 gap-x-3 items-center'>
+        {/* <ThemeButton /> */}
+        {session?.user ? <Link href={'/account/referral'} className='rounded-xl bg-white py-2.5 px-3 font-semibold font-mont'>
           Referral Code
         </Link> : ''}
 
