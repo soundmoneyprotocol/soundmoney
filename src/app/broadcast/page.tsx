89a088ae5b98e512888f41c8ec089042c624c84c
alt-franz <examdeskseries@gmail.com>
2024-02-29 15:38:16 +0000

    initial stream test

---
+++ src/app/broadcast/page.tsx
@@ -0,0 +1,16 @@
+import BroadcastStream from '@/components/BroadcastStream'
+import React from 'react'
+
+
+
+const page = () => {
+    return (
+        <>
+            <div className='w-96 h-96 flex justify-center items-center'>
+                <BroadcastStream />
+            </div>
+        </>
+    )
+}
+
+export default page
