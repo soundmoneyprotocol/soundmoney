2f65871da500d7150dd6ca69b79f7e1d7646f01a
soundmoneyprotocol <founder@soundmoneyprotocol.xyz>
2024-09-26 14:30:33 -0500

    Added Text to /swap & /play and centeredit TODO: Add Button to the links form 360NFT & Unitrap

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -6,13 +6,18 @@ import React from 'react';
 const Swap = () => {
   return (
     <>
-      <div className="mt-8 grid md:grid-cols-3 gap-6 pb-8">
-        <a
-          href="https://unitrap.vercel.app"
-          target="_blank"
-          rel="noreferrer"
-          className="px-5 py-12 items-center col-span-1 group border rounded-xl shadow-lg max-w-96 flex justify-between md:hover:scale-105  duration-200 hover:shadow-2xl"
-        ></a>
+      <div className="flex items-center justify-center min-h-screen">
+        <h2 className="textGradient md:text-7xl text-4xl font-gilroy text-center font-bold leading-relaxed">
+          Swap
+        </h2>
+        <div className="mt-8 grid md:grid-cols-3 gap-6 pb-8">
+          <a
+            href="https://unitrap.vercel.app"
+            target="_blank"
+            rel="noreferrer"
+            className="px-5 py-12 items-center col-span-1 group border rounded-xl shadow-lg max-w-96 flex justify-between md:hover:scale-105  duration-200 hover:shadow-2xl"
+          ></a>
+        </div>
       </div>
     </>
   );
