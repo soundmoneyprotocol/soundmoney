0379c03b4960b90177fd8775446bf52fb0dc70ff
eyram <pistisray@gmail.com>
2025-05-29 23:02:57 +0000

    build

--- src/components/Home/FAQs.tsx
+++ src/components/Home/FAQs.tsx
@@ -6,6 +6,7 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion"
 import { ArrowRight } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 
 
 export const FAQs = () => {
