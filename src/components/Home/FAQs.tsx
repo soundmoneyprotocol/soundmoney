b97122d136f7cc1a08f2b3b3d4d1da48b9de6060
soundmoneyprotocol <casmirconsults@gmail.com>
2025-06-14 22:01:32 +0100

    Updates: Unlock the Future FAQ

--- src/components/Home/FAQs.tsx
+++ src/components/Home/FAQs.tsx
@@ -25,7 +25,7 @@ export const FAQs = () => {
                         <AccordionItem value="item-1" className='bg-white rounded-2xl px-4'>
                             <AccordionTrigger className='text-2xl font-bold'>Own & Monetize Your Music</AccordionTrigger>
                             <AccordionContent>
-                                Yes. It adheres to the WAI-ARIA design pattern.
+                                Yes. You own your creations and YOU should retain the majority of the revenue.
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-2" className='bg-white rounded-2xl px-4'>
@@ -36,33 +36,33 @@ export const FAQs = () => {
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-3" className='bg-white rounded-2xl px-4'>
-                            <AccordionTrigger className='text-2xl font-bold'> Live Video Streaming & VOD </AccordionTrigger>
+                            <AccordionTrigger className='text-2xl font-bold'> Live AI Video Streaming & VOD </AccordionTrigger>
                             <AccordionContent>
-                                Yes. It&apos;s animated by default, but you can disable it if you prefer.
+                                Yes. We enable you to stream your music or creative content to your audience in real-time with AI-powered video.
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-4" className='bg-white rounded-2xl px-4'>
                             <AccordionTrigger className='text-2xl font-bold'> Launch Your Social Token </AccordionTrigger>
                             <AccordionContent>
-                                Yes. It&apos;s animated by default, but you can disable it if you prefer.
+                                Yes. You can launch your own social token and use it to interact with your audience.
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-5" className='bg-white rounded-2xl px-4'>
                             <AccordionTrigger className='text-2xl font-bold'>Real-Time Fan Interactions</AccordionTrigger>
                             <AccordionContent>
-                                Yes. It&apos;s animated by default, but you can disable it if you prefer.
+                                Yes. You can interact with your fans in real-time through live chats, comments, and more.
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-6" className='bg-white rounded-2xl px-4'>
                             <AccordionTrigger className='text-2xl font-bold'>Artist Support Foundation</AccordionTrigger>
                             <AccordionContent>
-                                Yes. It&apos;s animated by default, but you can disable it if you prefer.
+                                Yes. You can support your artists by providing them with direct support through BEZY C.A.R.E.S foundation.
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-7" className='bg-white rounded-2xl px-4'>
                             <AccordionTrigger className='text-2xl font-bold'>Digital Art Showcases</AccordionTrigger>
                             <AccordionContent>
-                                Yes. It&apos;s animated by default, but you can disable it if you prefer.
+                                Yes. You can showcase your digital art on our platform and reach a wider audience through our NFT drops and collaborations with other artists on the platform.
                             </AccordionContent>
                         </AccordionItem>
                     </Accordion>
